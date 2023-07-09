61b37b59e04df8c7a839f921be23206ae250f164
adityaj2202 <137844734+adityaj2202@users.noreply.github.com>
2023-07-09 09:06:19 -0400

    Create copy-files.ps1

---
+++ copy-files.ps1
@@ -0,0 +1,31 @@
+# Check if source and destination folder arguments are provided
+if ($args.Count -lt 2) {
+    Write-Host "Please provide source and destination folder paths as arguments."
+    Exit
+}
+
+# Get the source and destination folder paths from the arguments
+$sourceFolder = $args[0]
+$destinationFolder = $args[1]
+
+# Check if the source folder exists
+if (-not (Test-Path $sourceFolder -PathType Container)) {
+    Write-Host "Source folder not found: $sourceFolder"
+    Exit
+}
+
+# Check if the destination folder exists, if not, create it
+if (-not (Test-Path $destinationFolder -PathType Container)) {
+    New-Item -ItemType Directory -Path $destinationFolder | Out-Null
+    Write-Host "Destination folder created: $destinationFolder"
+}
+
+# Get the list of files to copy from the source folder
+$files = Get-ChildItem -Path $sourceFolder
+
+# Copy each file to the destination folder
+foreach ($file in $files) {
+    $destinationPath = Join-Path -Path $destinationFolder -ChildPath $file.Name
+    Copy-Item -Path $file.FullName -Destination $destinationPath
+    Write-Host "Copied file: $($file.Name)"
+}
